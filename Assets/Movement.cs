79451e0149385f95dd8363699417f5b3072bb92d
savior131 <91454389+savior131@users.noreply.github.com>
2023-07-27 00:40:36 +0300

    savior
    fixed some animation problems

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,21 +5,22 @@ using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
+
     [SerializeField] float acceleration = 10;
     [SerializeField] float speed=20;
     [SerializeField] float jumpForce = 15;
-    [SerializeField] Transform cam;
     [SerializeField] float sprintRate=1.5f;
     [SerializeField] float crouchRate=0.5f;
-    Animator anim;
 
-    float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-    float targetAngle;
-    float angle;
+    [SerializeField] Transform cameraTransform;
+
+    // Rotation speed multiplier
+    [SerializeField] float rotationSpeed = 5f;
+    Animator anim;
     float tempSpeed;
     bool isCrouching;
-    
+    float horizontal;
+    float vertical;
     string velocityX = "VelocityX";
     string velocityZ = "VelocityZ";
     string isCrouch = "IsCrouch";
@@ -31,30 +32,18 @@ public class Movement : MonoBehaviour
     {
         tempSpeed = speed;
         rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
     private void FixedUpdate()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        if (direction.magnitude >= 0.1f)
+        rb.AddRelativeForce(direction* acceleration, ForceMode.Force);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
 
-        {
-            
-
-            targetAngle =  cam.eulerAngles.y;
-            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f,angle, 0f);
-
-            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            rb.AddRelativeForce(direction* acceleration, ForceMode.Force);
-            
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
-
-           
-        }
-       
 
     }
     private void Update()
@@ -62,15 +51,30 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = tempSpeed * sprintRate;
+            if (horizontal != 0)
+            {
+                speed = tempSpeed * sprintRate * 0.5f;
+            }
+            if (vertical < 0)
+            {
+                speed = tempSpeed * sprintRate * 0.5f;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.C))
         {
             isCrouching = !isCrouching;
-           
         }
         else
         {
-            speed = tempSpeed;
+            if (horizontal != 0)
+            {
+                speed = tempSpeed * 0.5f;
+            }
+            if (vertical < 0)
+            {
+                speed = tempSpeed * 0.5f;
+            }
+            speed = Mathf.Lerp(speed, tempSpeed, Time.deltaTime*3);
         }
 
         if (isCrouching)
@@ -82,7 +86,7 @@ public class Movement : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
-       
+
 
         
         anim.SetFloat(velocityX, rb.velocity.x);
