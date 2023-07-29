cc88efe337bf3b94d810090f5f121e5ad4bbd4a8
savior131 <91454389+savior131@users.noreply.github.com>
2023-07-29 15:19:23 +0300

    savior
    a7a

--- Assets/player/Scripts/Movement.cs
+++ Assets/player/Scripts/Movement.cs
@@ -42,15 +42,10 @@ public class Movement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            rb.AddRelativeForce(moveDir * acceleration, ForceMode.Force);
-        }
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+      
+            rb.AddRelativeForce(direction * acceleration, ForceMode.Force);
+        
        
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
         xVel = transform.InverseTransformDirection(rb.velocity).x;
